0063b5bc942b23d61faaddd96622da760eef6359
clayton <crdcastro5@gmail.com>
2024-06-14 05:15:12 -0700

    typo

--- method1.py
+++ method1.py
@@ -2,7 +2,7 @@ from batch_processing import *
 batch_name = "method1"
 system_prompt = f"""
 You are an AI assistant that reads papers and extracts background information.
-Your audience are educcated in computer science, so point out the topics this paper focuses on,
+Your audience are educated in computer science, so point out the topics this paper focuses on,
 and which topics this paper was influenced by.
 """
 user_prompt = "Provide key background information for this paper."
